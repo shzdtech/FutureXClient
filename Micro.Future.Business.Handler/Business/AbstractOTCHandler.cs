d82dd4d7db887ddf1ddfc56c48541ea9758c60ea
zhaoliang.hu@yahoo.com <PassW0rd>
2016-10-03 12:58:33 +0800

    Add function

--- Micro.Future.Business.Handler/Business/AbstractOTCHandler.cs
+++ Micro.Future.Business.Handler/Business/AbstractOTCHandler.cs
@@ -9,8 +9,7 @@ using System.Threading.Tasks;
 
 namespace Micro.Future.Message
 {
-    public abstract class AbstractOTCHandler :
-        AbstractMessageHandler
+    public abstract class AbstractOTCHandler :  AbstractMessageHandler
     {
         protected void OnErrorAction(ExceptionMessage bizErr)
         {
