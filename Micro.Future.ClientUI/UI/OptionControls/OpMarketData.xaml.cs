e9037a4e3d7cd42304497e8600ee7a4d5949bb52
huzhaoliang <zhaoliang.hu@yahoo.com>
2016-11-12 16:54:02 +0800

    fix bug

--- Micro.Future.ClientUI/UI/OptionControls/OpMarketData.xaml.cs
+++ Micro.Future.ClientUI/UI/OptionControls/OpMarketData.xaml.cs
@@ -321,5 +321,64 @@ namespace Micro.Future.UI
 
             }
         }
+
+        private void underlyingCB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back")
+            {
+                //MessageBox.Show(this.volModelCB.SelectedItem.ToString());
+                this.underlyingCB.SelectedItem = null;
+            }
+        }
+
+        private void underlyingContractCB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back")  this.underlyingContractCB.SelectedItem = null; 
+        }
+
+        private void underlyingEX1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back") this.underlyingEX1.SelectedItem = null;
+        }
+
+        private void underlyingCB1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back") this.underlyingCB1.SelectedItem = null;
+        }
+
+        private void underlyingContractCB1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back") this.underlyingContractCB1.SelectedItem = null;
+        }
+
+        private void exchange1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back") this.exchange1.SelectedItem = null;
+        }
+
+        private void underlying1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back") this.underlying1.SelectedItem = null;
+        }
+
+        private void contract1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back") this.contract1.SelectedItem = null;
+        }
+
+        private void exchange2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back") this.exchange2.SelectedItem = null;
+        }
+
+        private void underlying2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back") this.underlying2.SelectedItem = null;
+        }
+
+        private void contract2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Back") this.contract2.SelectedItem = null;
+        }
     }
 }
