49223852d0ceb22f93071795c81cf4bc7fcd037d
rainmilk <rainmilk@gmail.com>
2017-06-10 17:39:17 +1000

    Update autoupdater project

--- Micro.Future.AutoUpdate/MainWindow.xaml.cs
+++ Micro.Future.AutoUpdate/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AutoUpdaterDotNET;
 
 namespace Micro.Future.AutoUpdate
 {
@@ -23,6 +24,7 @@ namespace Micro.Future.AutoUpdate
         public MainWindow()
         {
             InitializeComponent();
+            AutoUpdater.Start("http://rbsoft.org/updates/AutoUpdaterTest.xml");
         }
     }
 }
